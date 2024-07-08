bc6149bb133a91b9b0b53118dd08a3a75667d39c
Hudson <hudson.s19072006@gmail.com>
2024-07-08 20:31:37 -0300

    edit footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     return (
         <div className="p-3 mt-24">
             <footer className="m-28 flex text-center justify-center border-b-slate-700">
-                <p className="italic">&copy; Equizzi Desenvolvimentos</p>
+                <p className="italic text-gray-700">&copy; Equizzi Desenvolvimentos</p>
             </footer>
             <hr />
             
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div>
-            <img src="src/imgs/logo_1.png" alt="" />
+            <img src="https://github.com/HudsonSilv2/react-com-vite/blob/main/src/imgs/logo_1.png" alt="logo-principal" />
         </div>
     );
 }
