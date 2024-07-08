08eadecfc04caba1339e6eefcc1ac57e95dec2f3
Hudson <hudson.s19072006@gmail.com>
2024-07-08 20:23:14 -0300

    image direct for desktop

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div>
-            <img src="./react-com-vite/public/logo_1.png" alt="" />
+            <img src=".c:\Users\hudso\Desktop\projetosReact\react-com-vite\public\logo_1.png" alt="" />
         </div>
     );
 }
