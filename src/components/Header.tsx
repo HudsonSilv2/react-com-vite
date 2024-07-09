d0c41784ebd6c13402f3e84d9068bd2c5fce2fac
Hudson <hudson.s19072006@gmail.com>
2024-07-08 22:49:18 -0300

    mmn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,8 @@
-import { Logo } from "./Logo";
 
 export const Header = () => {
     return (
         <div className="p-2 m-3">
             <ul className=" text-gray-700 flex items-center justify-evenly ">
-
-                <div className="cursor-pointer">
-                    <Logo />
-                </div>
 
                 <li className="p-2 h-10 w-20 text-center border border-gray-400 rounded-full hover:bg-gray-400 hover:text-white cursor-pointer">Sobre</li>
                 <li className="p-2 h-10 w-20 text-center border border-gray-400 rounded-full hover:bg-gray-400 hover:text-white cursor-pointer">Contatos</li>
